2504f7b1e9b5a9dbb69a5c15d1c21cbac1b07f0a
Pedro Pupo Sa da Costa <pedro@hublo.com>
2023-04-27 22:55:43 +0200

    chore: config traditional pipeline

--- libs/next/components/src/components/home-page/landing-screen.tsx
+++ libs/next/components/src/components/home-page/landing-screen.tsx
@@ -14,74 +14,74 @@ const PUNCH_LINES = [
 export const LandingScreen: FunctionComponent = () => {
   return (
     <Box
+      sx={{
+        m: "auto",
+        p: 2,
+        height: "100%",
+        width: "100%",
+        maxWidth: "950px",
+        display: "flex",
+        zIndex: 10,
+        flexDirection: { xs: "column-reverse", md: "row" },
+      }}
+    >
+      <Box
         sx={{
-          m: "auto",
-          p: 2,
           height: "100%",
-          width: "100%",
-          maxWidth: "950px",
+          my: "auto",
           display: "flex",
-          zIndex: 10,
-          flexDirection: { xs: "column-reverse", md: "row" },
+          flexDirection: "column",
+          maxHeight: {
+            xs: "100%",
+            md: "240px",
+          },
+          width: {
+            xs: "100%",
+            md: "50%",
+          },
         }}
       >
+        <Typography
+          level="h3"
+          component="h1"
+          sx={{ fontWeight: 900, fontSize: { md: "3em" } }}
+        >
+          Chair Flight is <br />
+          <Typical sx={{ color: "primary.500" }} steps={PUNCH_LINES} />
+          &nbsp;
+        </Typography>
+        <Typography level="h4" component="h2">
+          Built by students for students.
+        </Typography>
+        <Box sx={{ flex: 1 }} />
         <Box
           sx={{
-            height: "100%",
-            my: "auto",
             display: "flex",
-            flexDirection: "column",
-            maxHeight: {
-              xs: "100%",
-              md: "240px",
-            },
-            width: {
-              xs: "100%",
-              md: "50%",
+            flexDirection: {
+              xs: "column",
+              md: "row",
             },
           }}
         >
-          <Typography
-            level="h3"
-            component="h1"
-            sx={{ fontWeight: 900, fontSize: { md: "3em" } }}
-          >
-            Chair Flight is <br />
-            <Typical sx={{ color: "primary.500" }} steps={PUNCH_LINES} />
-            &nbsp;
-          </Typography>
-          <Typography level="h4" component="h2">
-            Built by students for students.
-          </Typography>
-          <Box sx={{ flex: 1 }} />
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: {
-                xs: "column",
-                md: "row",
-              },
-            }}
-          >
-            <Button
-              sx={{ my: 1, mr: { md: 2 } }}
-              fullWidth
-              size="lg"
-              component={Link}
-              href="/questions"
-              children="Explore Questions"
-            />
-            <Button
-              sx={{ my: 1 }}
-              fullWidth
-              size="lg"
-              variant="outlined"
-              component={Link}
-              href="/articles/about-us"
-              children="About This Project"
-            />
-          </Box>
+          <Button
+            sx={{ my: 1, mr: { md: 2 } }}
+            fullWidth
+            size="lg"
+            component={Link}
+            href="/questions"
+            children="Explore Questions"
+          />
+          <Button
+            sx={{ my: 1 }}
+            fullWidth
+            size="lg"
+            variant="outlined"
+            component={Link}
+            href="/articles/about-us"
+            children="About This Project"
+          />
         </Box>
       </Box>
+    </Box>
   );
 };
