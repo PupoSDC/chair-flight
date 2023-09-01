089166b087424a0ad2762133e091231994120fa5
Pedro Pupo Sa da Costa <pedro@hublo.com>
2023-09-02 00:57:29 +0100

    chore: bump deps

--- libs/react/containers/src/__tests__/setup-tests.ts
+++ libs/react/containers/src/__tests__/setup-tests.ts
@@ -1,6 +1,6 @@
 import * as matchers from "@testing-library/jest-dom/matchers";
 import { cleanup } from "@testing-library/react";
-import { afterEach, expect, vi } from "vitest";
+import { afterEach, beforeAll, expect, vi } from "vitest";
 
 expect.extend(matchers);
 
