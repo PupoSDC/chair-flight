f1f8b7547b480917570d92075b586e08ad7e40d1
Pedro Pupo Sa da Costa <pedro.pupo@ist.utl.pt>
2024-02-06 12:35:49 +0000

    fix: add missing file

---
+++ libs/core/question-bank/src/tests/get-number-of-available-questions.ts
@@ -0,0 +1,26 @@
+import type { LearningObjectiveId } from "@chair-flight/core/question-bank";
+
+export const getNumberOfAvailableQuestions = (
+  subject: {
+    learningObjectives: Array<{
+      id: LearningObjectiveId;
+      numberOfQuestions: number;
+      learningObjectives: Array<{
+        id: LearningObjectiveId;
+        numberOfQuestions: number;
+      }>;
+    }>;
+  },
+  selectedLearningObjectives: LearningObjectiveId[],
+) => {
+  return subject.learningObjectives.reduce((sum, lo) => {
+    if (selectedLearningObjectives.includes(lo.id)) {
+      return sum + lo.numberOfQuestions;
+    }
+    lo.learningObjectives.forEach((lo2) => {
+      if (!selectedLearningObjectives.includes(lo2.id)) return;
+      sum += lo2.numberOfQuestions;
+    });
+    return sum;
+  }, 0);
+};
